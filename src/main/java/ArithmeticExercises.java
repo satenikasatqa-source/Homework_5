e02f38af801f38563dcf9d086e97e80590125223
Saten Asatryan <satenik.asat.qa@gmail.com>
2025-11-22 21:28:26 +0400

    Add arithmetic exercises

---
+++ src/main/java/ArithmeticExercises.java
@@ -0,0 +1,21 @@
+public class ArithmeticExercises {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 8;
+       double c= 2.0;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(b/c);
+        System.out.println(c%b);
+        System.out.println(a>b);
+        System.out.println(b>c);
+        System.out.println(b%c+a);
+        System.out.println((b/c+a));
+        System.out.println((b+c)<=a);
+        System.out.println(a*c+b);
+}
+}
