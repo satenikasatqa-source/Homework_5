bde2afb25b492b9e6b0cd21fc8ae2e36432a5486
Saten Asatryan <satenik.asat.qa@gmail.com>
2025-11-25 21:20:26 +0400

    Add overflowExample() method and integrate overflow demonstration

--- src/main/java/ArithmeticExercises.java
+++ src/main/java/ArithmeticExercises.java
@@ -3,19 +3,28 @@ public class ArithmeticExercises {
     public static void main(String[] args) {
         int a = 5;
         int b = 8;
-       double c= 2.0;
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
-        System.out.println(a%b);
-        System.out.println(b/c);
-        System.out.println(c%b);
-        System.out.println(a>b);
-        System.out.println(b>c);
-        System.out.println(b%c+a);
-        System.out.println((b/c+a));
-        System.out.println((b+c)<=a);
-        System.out.println(a*c+b);
-}
+        double c = 2.0;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+        System.out.println(b / c);
+        System.out.println(c % b);
+        System.out.println(a > b);
+        System.out.println(b > c);
+        System.out.println(b % c + a);
+        System.out.println((b / c + a));
+        System.out.println((b + c) <= a);
+        System.out.println(a * c + b);
+    }
+
+    public static void overflowExample() {
+        int a = Integer.MAX_VALUE;
+        System.out.println("a (max int): " + a);
+
+        int overflow = a + 1;
+        System.out.println("After overflow (a + 1): " + overflow);
+    }
+
 }
